2592fc08809899529c1220b1c069a0e51f1397b0
shaikJaleelBasha <jaleelbashashaik18@gmail.com>
2024-10-14 20:56:50 +0530

    main

---
+++ src/main/java/com/jaleel/springdemo/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.jaleel.springdemo.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+   @GetMapping
+    public List<String> findAllStudents() {
+        return List.of(
+            "Jaleel",
+            "Basha"
+        );
+    }
+}
